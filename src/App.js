0602b6af13e79b6bd8b8e8ae2bd106612e178b39
Ashwin1729 <mathematicswroteus@gmail.com>
2023-07-15 14:46:29 +0530

    initial configurations and tailwind integration

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from "react";
 
 function App() {
-  return <div>Hello World!</div>;
+  return (
+    <div className="flex">
+      <div className="#NavBar flex-none w-40 text-center">
+        <p>Nav Bar</p>
+      </div>
+      <div className="#musicList grow text-center">
+        <p>Music List</p>
+      </div>
+      <div className="#musicPlayer flex-none w-1/2 text-center">
+        <p>Music Player</p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
