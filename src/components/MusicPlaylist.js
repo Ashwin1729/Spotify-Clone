e011620a7c826b95f1143b9af545b641059492f5
Ashwin1729 <mathematicswroteus@gmail.com>
2023-07-19 13:04:35 +0530

    skeleton loading added

--- src/components/MusicPlaylist.js
+++ src/components/MusicPlaylist.js
@@ -17,21 +17,6 @@ const GET_SONGS = gql`
   }
 `;
 
-// const skeletonCard = (
-//   <li className="flex items-center justify-between my-2 w-full hover:bg-gray-600 cursor-pointer p-3 rounded-lg duration-500 animate-pulse">
-//     <div className="flex items-center space-x-5">
-//       <div className="w-12 h-12 rounded-full">
-//         <div className="w-full h-full rounded-full bg-slate-200"></div>
-//       </div>
-//       <div className="flex flex-col text-left text-white justify-center">
-//         <div className="h-4 mb-2 bg-slate-200 rounded w-60"></div>
-//         <div className="h-2 bg-slate-200 rounded w-32"></div>
-//       </div>
-//     </div>
-//     <div className="h-3 bg-slate-200 rounded w-10"></div>
-//   </li>
-// );
-
 let skeleton = [];
 
 for (let i = 0; i < 10; i++) {
